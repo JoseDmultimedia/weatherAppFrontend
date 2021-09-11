c10f3e48279199d33ebf920bbd22630b02516f33
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2021-09-11 02:40:05 -0500

    Se añaden las rutas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import WeatherComponent from './components/weather-component/weatherComponent.js';
 import InicioComponent from './components/inicio-app/inicioApp.js'
@@ -6,8 +8,18 @@ import InicioComponent from './components/inicio-app/inicioApp.js'
 function App() {
   return (
     <div className="o-container-all">
-      {/* <WeatherComponent></WeatherComponent> */}
-          <InicioComponent></InicioComponent>
+      
+      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <InicioComponent></InicioComponent>
+          </Route>
+          <Route path="/weather">
+            <WeatherComponent></WeatherComponent>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/inicio-app/inicioApp.js
+++ src/components/inicio-app/inicioApp.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./inicioApp.css";
 
 function inicioApp() {
     return (
-        <div>
-            Hola mundo
+        <div className="o-inicio-card">
+            <h1 className="o-text-inicio">Weather App</h1>
+            <h3 className="o-text-inicio">Ingresa la Key</h3>
+            <input type="text" placeholder="Numero de key" className="o-input-inicio"></input>
+            <button className="o-btn-inicio">Ingresar</button>
         </div>
     )
 }
