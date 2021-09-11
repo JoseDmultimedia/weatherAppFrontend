bb4a8d937dea57011215232251f8c100ca03c53b
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2021-09-10 23:36:04 -0500

    Se arregla diagramacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import WeatherComponent from './components/weather-component/weatherComponent.js';
+import InicioComponent from './components/inicio-app/inicioApp.js'
 
 
 function App() {
   return (
     <div className="o-container-all">
-      <WeatherComponent></WeatherComponent>
+      {/* <WeatherComponent></WeatherComponent> */}
+          <InicioComponent></InicioComponent>
     </div>
   );
 }
--- src/components/inicio-app/inicioApp.js
+++ src/components/inicio-app/inicioApp.js
@@ -3,7 +3,7 @@ import React from 'react'
 function inicioApp() {
     return (
         <div>
-            
+            Hola mundo
         </div>
     )
 }
